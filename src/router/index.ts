5bee3523d6f61743a880b34d641afda94148075a
nao-kichi <98793084+nao-kichi@users.noreply.github.com>
2025-04-01 12:00:00 +0900

    index.tsを追加しました。

---
+++ src/router/index.ts
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LevelSelectionView from '../views/LevelSelectionView.vue';
+import LoginView from '../components/LoginView.vue';
+import RegisterView from '../components/RegisterView.vue';
+import MyPageView from '../components/MyPageView.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/levels',
+      name: 'levels',
+      component: LevelSelectionView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: MyPageView,
+    },
+  ],
+});
+
+export default router;
+
